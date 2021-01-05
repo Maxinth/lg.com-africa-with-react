06238c171ba47127cff1e12c659228e286308a5c
maxinth <tunde.j.adeleye@gmail.com>
2021-01-05 09:48:52 +0100

    TabWithSlides all done

--- src/components/TabWithSlides/ItemsTitleTab.js
+++ src/components/TabWithSlides/ItemsTitleTab.js
@@ -6,15 +6,13 @@ import "./itemsTitleTab.css";
 // let slideValue = 0;
 
 const ItemsTitleTab = () => {
-  // useReducer
-
-  // slideTabState
-  // let slideValue = '';
+  // useReducer to monitor the translation due to clicks on the control arrows
 
   // action types
   const actionType = {
     forward: "FORWARD",
     backward: "BACKWARD",
+    // reset: "RESET",
   };
 
   const initialState = 25;
@@ -25,39 +23,45 @@ const ItemsTitleTab = () => {
 
       case actionType.backward:
         return state - 20;
+      // case actionType.reset:
+      //   return 0;
       default:
         return state;
     }
   };
 
   const [val, dispatch] = useReducer(reducer, initialState);
+
+  // state to monitor the number of clicks - and disable the arrows based on the respective specified value
   const [clickCount, setClickCount] = useState(0);
+
+  // increase count and slide tab forward
   const forwardClick = () => {
     setClickCount((clickCount) => clickCount + 1);
-    // if (clickCount < 3) {
-    dispatch(actionType.forward);
-    console.log("forward dispatched");
-    // }
-  };
-  const backwardClick = () => {
-    setClickCount((clickCount) => clickCount - 1);
-    // if (clickCount >= 0) {
-    dispatch(actionType.backward);
-    console.log("back dispatched");
-    // }
+    dispatch(actionType.forward); // slide tab by specified value
   };
 
+  // decrease count and slide tab backward
+  const backwardClick = () => {
+    setClickCount((clickCount) => clickCount - 1);
+    dispatch(actionType.backward); // slide tab by specified value
+  };
+
+  // function to translate the tab based on the value.
   const translateItems = (val) => {
     return `translateX(${val}%)`;
   };
 
+  // run the function every time val changes
   useEffect(() => {
     translateItems();
   }, [val]);
-  // console.log("slideValue = ", slideValue);
-  console.log("val = ", val);
 
-  console.log("clickCount = ", clickCount);
+  // state to track which item was clicked
+  const [itemClicked, setItemClicked] = useState("mostPopular");
+  const handleClick = (id) => setItemClicked(id);
+  const colorByClick = (id) => (itemClicked === id ? "lg" : "");
+
   return (
     <section className="itemsTab">
       <div className="itemsTab__container">
@@ -73,13 +77,31 @@ const ItemsTitleTab = () => {
           className="itemsTab__items"
           style={{ transform: translateItems(val) }}
         >
-          <span className="itemsTab__item">
+          <span
+            className={`itemsTab__item ${colorByClick("mostPopular")}`}
+            onClick={() => handleClick("mostPopular")}
+          >
             <span className="mod">most</span>
             popular
           </span>
-          <span className="itemsTab__item">exciting new offers for you</span>
-          <span className="itemsTab__item">newest</span>
-          <span className="itemsTab__item">highly rated</span>
+          <span
+            className={`itemsTab__item ${colorByClick("newOffers")}`}
+            onClick={() => handleClick("newOffers")}
+          >
+            exciting new offers for you
+          </span>
+          <span
+            className={`itemsTab__item ${colorByClick("newest")}`}
+            onClick={() => handleClick("newest")}
+          >
+            newest
+          </span>
+          <span
+            className={`itemsTab__item ${colorByClick("highlyRated")}`}
+            onClick={() => handleClick("highlyRated")}
+          >
+            highly rated
+          </span>
         </section>
         <button
           className="itemsTab__controlsContainer"
